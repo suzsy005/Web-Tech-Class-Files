adf03c0018e77260cac6cf189ac656db4edc3b7c
suzsy005 <suzsy005@mymail.unisa.edu.au>
2025-11-12 15:32:39 +1030

    Reviewing week 11 contens(in progress)

--- aowebapp/aowebapp/src/App.js
+++ aowebapp/aowebapp/src/App.js
@@ -18,6 +18,11 @@ function App() {
           Learn React
         </a>
       </header>
+      <div className="App container">
+          <div className="bg-light py-1 mb-2">
+              <h2 className="text-center">Example Application</h2>
+          </div>
+      </div>
     </div>
   );
 }
